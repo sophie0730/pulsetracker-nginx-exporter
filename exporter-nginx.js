4bc1644270f65e0091ffc852078d1ee757a8df76
Sophie <sophie010017@gmail.com>
2023-12-25 13:18:32 +0800

    feat: move nginx exporter to an independent repo

---
+++ exporter-nginx.js
@@ -0,0 +1,1 @@
+import * as applicationExporter from './exporters/application.js';
---
+++ utils/influxdb-util.js
@@ -0,0 +1,17 @@
+import { findUp } from 'find-up';
+import dotenv from 'dotenv';
+
+export let INFLUXDB_URL, ORG, BUCKET, MEASUREMENT, ALERT_MEASUREMENT, TOKEN, WRITE_API_URL;
+
+(async () => {
+  const dotenvPath = await findUp('.env');
+  dotenv.config({ path: dotenvPath });
+
+  INFLUXDB_URL = process.env.INFLUXDB_URL;
+  ORG = process.env.ORG;
+  BUCKET = process.env.BUCKET;
+  MEASUREMENT = process.env.MEASUREMENT;
+  ALERT_MEASUREMENT = process.env.ALERT_MEASUREMENT;
+  TOKEN = process.env.TOKEN;
+  WRITE_API_URL = `${process.env.INFLUXDB_URL}/api/v2/write?org=${process.env.ORG}&bucket=${process.env.BUCKET}&precision=ns`;
+})();
